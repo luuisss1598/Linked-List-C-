1aad0b75e6835fdbe0f0c7c19e0d548ebdec3cac
luuisss <luisherrera722@gmail.com>
2020-08-20 13:39:11 -0700

    Simple linked list created

---
+++ linked_list.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int info;
+        Node* move;
+};
+
+void showList(Node* show);
+
+int main()
+{
+    Node* head = nullptr;
+    Node* middle = nullptr;
+    Node* last = nullptr;
+
+    head = new Node();
+    middle = new Node();
+    last = new Node();
+
+    head->info = 1;
+    head->move = middle;
+
+    middle->info = 2;
+    middle->move = last;
+
+    last->info = 3;
+    last->move = NULL;
+
+    showList(head);
+
+}
+
+void showList(Node* xx)
+{
+    cout << endl;
+    while (xx != NULL)
+    {
+        cout << xx->info << " ";
+        xx = xx->move;
+
+    }
+    cout << endl;
+}
